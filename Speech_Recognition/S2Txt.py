50b7116948810b23247416a23a3bf078380c8f48
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2020-08-08 18:42:34 +0530

    Add files via upload

---
+++ Speech_Recognition/S2Txt.py
@@ -0,0 +1,60 @@
+import speech_recognition as sr
+import time
+import playsound
+import os
+from gtts import gTTS
+
+
+
+def speak(text):
+    tts = gTTS(text = text , lang = "en")
+    filename = "voice3.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+    os.remove(filename)
+    
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception:" +str(e))
+            
+        return said
+            
+            
+text = get_audio()
+
+
+if "hello" in text:
+    speak("hello how are you?")
+    
+if "what is your name " in text:
+    speak("My name is pyto")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
